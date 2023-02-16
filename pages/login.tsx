69cddeb33c308f2294d21692a529832d4466cc65
Layan Oliveira <layan_oliveira@hotmail.com>
2023-02-16 13:52:17 -0300

    Added Login Page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,6 +1,22 @@
+import Head from "next/head"
+import Image from "next/image"
+
 function Login() {
   return (
-    <div>Login</div>
+    <div className="relative flex h-screen w-screen flex-col bg-black md:items-center
+    md:justify-center md:bg-transparent">
+      <Head>
+        <title>ContentBay</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Image
+      src="/login/movieposterbg.jpg"
+      alt="Logo"
+      fill
+      className="-z-10 hidden opacity-60 sm:inline"
+      object-fit="cover"
+      />
+    </div>
   )
 }
 

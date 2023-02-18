3709c8fd2aee03dcb946441b97f7ad04c000c3c7
Layan Oliveira <layan3210@gmail.com>
2023-02-17 22:57:40 -0300

    Added trailers for individual thumbnails

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,25 +1,34 @@
 import Image from "next/image"
+import { useRecoilState } from "recoil"
+import { modalState, movieState } from "../atoms/modalAtoms"
 import { Movie } from "../typings"
 
 interface Props {
-    // When using Firebase
-    // movie: Movie | DocumentData
-    movie: Movie
+  // When using Firebase
+  // movie: Movie | DocumentData
+  movie: Movie
 }
 
-function Thumbnail({movie}: Props) {
+function Thumbnail({ movie }: Props) {
+  const [showModal, setShowModal] = useRecoilState(modalState)
+  const [currentMovie, setCurrentMovie] = useRecoilState(movieState)
+
   return (
     <div className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 
-    ease-out md:h-36 md:min-w-[260px] md:hover:scale-105">
-        <Image
-            src={`https://image.tmdb.org/t/p/w500${
-            movie.backdrop_path || movie.poster_path
-            }`}
-            className="rounded-sm object-cover md:rounded"
-            fill
-            alt="Thumbnail"
-        />
-  </div>
+    ease-out md:h-36 md:min-w-[260px] md:hover:scale-105"
+      onClick={() => {
+        setCurrentMovie(movie)
+        setShowModal(true)
+      }}
+    >
+      <Image
+        src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path || movie.poster_path
+          }`}
+        className="rounded-sm object-cover md:rounded"
+        fill
+        alt="Thumbnail"
+      />
+    </div>
   )
 }
 

9f1ee5d1d8d9ead2759693b3904bb934c1e1399c
Layan Oliveira <layan3210@gmail.com>
2023-02-19 20:23:04 -0300

    Hub in construction

--- components/Hub.tsx
+++ components/Hub.tsx
@@ -31,31 +31,36 @@ function Hub() {
             </header>
 
             <main className="pt-28 max-w-5xl px-5 pb-12 transition-all md:px-10">
+                <div className="mt-4 flex flex-col space-y-4">
+                    <div className="flex w-full items-center justify-center self-end md:w-3/5">
+                        <div className="HubBox">MyFlix</div>
+                        <div className="HubBox">Musify</div>
+                        <div className="HubBox">ComicBay</div>
+                        <div className="HubBox">NovelsBay</div>
+                    </div>
+                    <div
+                        className="flex justify-center">Pick your poison.
+                    </div>
+                </div>
+
                 <h1 className="mb-3 text-3xl font-medium">
                     Choose the plan that's right for you
                 </h1>
-                <ul>
-                    <li className="flex items-center gap-x-2 text-lg">
-                        <CheckIcon className="h-7 w-7 text-[#E50914]" /> Watch all you want.
-                        Ad-free.
-                    </li>
-                    <li className="flex items-center gap-x-2 text-lg">
-                        <CheckIcon className="h-7 w-7 text-[#E50914]" /> Recommendations
-                        just for you.
-                    </li>
-                    <li className="flex items-center gap-x-2 text-lg">
-                        <CheckIcon className="h-7 w-7 text-[#E50914]" /> Change or cancel
-                        your plan anytime.
-                    </li>
-                </ul>
-                <div className="mt-4 flex flex-col space-y-4">
-                    <div className="flex w-full items-center justify-center self-end md:w-3/5">
-                        <div className="planBox">MyFlix</div>
-                        <div className="planBox">Musify</div>
-                        <div className="planBox">ComicBay</div>
-                        <div className="planBox">NovelsBay</div>
-                    </div>
-                    <button>Subscribe</button>
+                <div>
+                    <ul>
+                        <li className="flex items-center gap-x-2 text-lg">
+                            <CheckIcon className="h-7 w-7 text-[#E50914]" /> Watch all you want.
+                            Ad-free.
+                        </li>
+                        <li className="flex items-center gap-x-2 text-lg">
+                            <CheckIcon className="h-7 w-7 text-[#E50914]" /> Recommendations
+                            just for you.
+                        </li>
+                        <li className="flex items-center gap-x-2 text-lg">
+                            <CheckIcon className="h-7 w-7 text-[#E50914]" /> Change or cancel
+                            your plan anytime.
+                        </li>
+                    </ul>
                 </div>
             </main>
         </div>

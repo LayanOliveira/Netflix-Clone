7970eea3e40bfcc89f3829b037cf9a65147ec390
Layan Oliveira <layan3210@gmail.com>
2023-02-20 12:37:47 -0300

    redirect to main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,9 +41,9 @@ const Home = ({
   const showModal = useRecoilValue(modalState)
   const subscription = false
 
-  if (loading || subscription === null) return null
+  // if (loading || subscription === null) return null
 
-  if (!subscription) return <Hub/>
+  // if (!subscription) return <Hub/>
 
   return (
     <div className={`relative h-screen bg-gradient-to-b lg:h-[140vh] ${showModal && '!h-screen overflow-hidden'}`}>
